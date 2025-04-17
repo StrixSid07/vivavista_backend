df45a68d697d2cac6208bdd1a6b6deda1a720642
Genius Graphics & Infotech <geniusgraphicsdev07@gmail.com>
2025-04-18 04:50:43 +0530

    Update faqsRoutes.js

--- routes/faqsRoutes.js
+++ routes/faqsRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const faqsController = require("../controllers/faqsController");
+const { protect, isAdmin } = require("../middleware/authMiddleware");
 
-router.post("/", faqsController.createFaq);
+router.post("/", protect, isAdmin, faqsController.createFaq);
 router.get("/", faqsController.getAllFaqs);
 router.post("/bulk", faqsController.createMultipleFaqs);
 router.get("/:id", faqsController.getFaqById);
-router.put("/:id", faqsController.updateFaq);
+router.put("/:id", protect, isAdmin, faqsController.updateFaq);
 router.delete("/:id", faqsController.deleteFaq);
 
 module.exports = router;

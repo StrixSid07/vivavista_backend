03b247e4326afe51dbaee02c6c480db6cf2dc6af
Genius Graphics & Infotech <geniusgraphicsdev07@gmail.com>
2025-04-22 02:31:32 +0530

    Update homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -16,6 +16,7 @@ const {
 } = require("../controllers/homeController");
 const { upload, uploadToS3 } = require("../middleware/imageUpload");
 const router = express.Router();
+const { protect, isAdmin } = require("../middleware/authMiddleware");
 
 router.get("/deals/featured", getFeaturedDeals);
 router.get("/destinations", getDestinations);
@@ -23,11 +24,11 @@ router.get("/destinations/popular", getPopularDestinations);
 router.get("/reviews", getReviews);
 router.get("/blogs", getBlogs);
 router.get("/blogs/:id", getBlogById);
-router.post("/blogs", upload.single("images"), addBlog);
+router.post("/blogs", upload.single("images"), isAdmin, protect, addBlog);
 router.get("/blogs/latest", getLatestBlogs);
 router.get("/homepage", getHomepageData);
 router.post("/subscribe-newsletter", subscribeNewsletter);
-router.delete("/image/:blogId", deleteBlogImage);
-router.put("/:id", upload.single("images"), updateBlog); // 🔄 Update
-router.delete("/blogs/:id", deleteBlog);
+router.delete("/image/:blogId", isAdmin, protect, deleteBlogImage);
+router.put("/:id", upload.single("images"), isAdmin, protect, updateBlog); // 🔄 Update
+router.delete("/blogs/:id", isAdmin, protect, deleteBlog);
 module.exports = router;
